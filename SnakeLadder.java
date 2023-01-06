f41e38d5738dec10d888c50f499167090ea57306
pratikshabathe <pratikshabathe11@gmail.com>
2023-01-06 15:11:34 +0530

    Added: player at position 0

---
+++ SnakeLadder.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class SnakeLadder {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Starting of snake and ladder game");
+		
+		int position = 0;
+		System.out.println("The starting position of the player is : " +position);
+		
+	}
+}
